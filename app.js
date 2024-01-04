93ca04668b672aa027493cbbb7cbb98474b2c40b
bowen-anth <bowen.anth@gmail.com>
2024-01-03 23:48:36 -0700

    about done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('test')
+const xValues = ["Morbi ligula", "Scelerisque", "Praesent maecenas", "Morbi ligula", "Siekd eked"];
+const yValues = [41, 29, 23, 4, 3];
+const barColors = ["#B969C8", "#EA417C","#EE6B03","#FCD842","#68F0B0"];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: true},
+    title: {
+      display: true
+    }
+  }
+});
